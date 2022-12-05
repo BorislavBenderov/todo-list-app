bd32f4fe82568633d8b61349cd4dfdb117f05ebb
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-05 16:40:06 +0200

    Add check/uncheck option on todo

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,16 +1,36 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
+import { database } from "../../firebaseConfig";
 import { DeleteTodo } from "./DeleteTodo";
 
 export const Todo = ({ todo, onEditHandler }) => {
+    const [isChecked, setIsCkecked] = useState(todo.checked);
+
+    const onChangeCheck = async () => {
+        try {
+            await updateDoc(doc(database, 'todos', todo.id), {
+                checked: !todo.checked
+            });
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return (
         <div className="todo">
             <div className="todo__title">
-                <p>{todo.title}</p>
+                <p style={isChecked ? { textDecoration: 'line-through' } : { textDecoration: 'none' }}>{todo.title}</p>
             </div>
             <div className="todos__btns">
                 <label htmlFor="check"></label>
-                <input type="checkbox" name="check" id="check" />
+                <input type="checkbox"
+                    name="check" id="check"
+                    checked={isChecked}
+                    onChange={() => setIsCkecked(!todo.checked)}
+                    onClick={onChangeCheck}
+                />
                 <button onClick={() => onEditHandler(todo.id)}>x</button>
-                <DeleteTodo todoId={todo.id}/>
+                <DeleteTodo todoId={todo.id} />
             </div>
         </div>
     );
