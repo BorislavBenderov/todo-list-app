60647ecfe8f961cc9ae5b77948363951d14c88c1
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-05 19:02:51 +0200

    Delete user context

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Register } from './components/auth/Register';
 import { Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { Todos } from './components/todos/Todos';
-import { UserContextProvider } from './contexts/UserContext';
 import { TodoContextProvider } from './contexts/TodoContext';
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-        <UserContextProvider>
           <TodoContextProvider>
             <Routes>
               <Route path='/' element={<Login />} />
@@ -18,7 +16,6 @@ function App() {
               <Route path='/todos' element={<Todos />} />
             </Routes>
           </TodoContextProvider>
-        </UserContextProvider>
       </AuthContextProvider>
     </div>
   );
