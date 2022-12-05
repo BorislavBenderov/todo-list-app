cbc02814d2a317585dfa30fc1e0093a9bab25e17
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-05 10:09:07 +0200

    Add possibility users to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,38 @@
 import { Link } from "react-router-dom";
+import { browserLocalPersistence, setPersistence, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
 
 export const Login = () => {
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email === '' || password === '') {
+            alert('Please fill all the fields!');
+            return;
+        }
+
+        setPersistence(auth, browserLocalPersistence)
+            .then(() => {
+                signInWithEmailAndPassword(auth, email, password)
+                    .then(() => {
+                        console.log('ok');
+                    })
+                    .catch((err) => {
+                        alert(err.message);
+                    })
+            })
+    }
+
     return (
         <div className="auth">
             <div className="auth__container">
                 <h1>ToDo List</h1>
-                <form className="auth__form">
+                <form className="auth__form" onSubmit={onLogin}>
                     <label htmlFor="email" />
                     <input type="text" placeholder="Email" id="email" name="email" />
                     <label htmlFor="password" />
