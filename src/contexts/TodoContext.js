79b0053b6813283aecbe9a985b06955dabd9bef6
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-05 19:05:32 +0200

    Show lastly created todo on top

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,4 +1,4 @@
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { createContext, useEffect, useState } from "react";
 import { database } from "../firebaseConfig";
 
@@ -8,7 +8,8 @@ export const TodoContextProvider = ({ children }) => {
     const [todos, setTodos] = useState([]);
 
     useEffect(() => {
-        onSnapshot(collection(database, 'todos'), (snapshot) => {
+        const q = query(collection(database, 'todos'), orderBy ("timestamp", "desc"))
+        onSnapshot(q, (snapshot) => {
             setTodos(snapshot.docs.map((item) => {
                 return { ...item.data(), id: item.id }
             }));
