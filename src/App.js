756419a6e83fdbbe3a67266d5f2ca6db8edd593d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-05 12:34:29 +0200

    Add Todos component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 import { Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
+import { Todos } from './components/todos/Todos';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/todos' element={<Todos />} />
         </Routes>
       </AuthContextProvider>
     </div>
---
+++ src/components/todos/Todos.js
@@ -0,0 +1,50 @@
+export const Todos = () => {
+    return (
+        <div className="todos">
+            <section className="add__todo">
+                <h1>ToDo App</h1>
+                <form className="add__todo__form">
+                    <label htmlFor="todo"></label>
+                    <input type="text" id="todo" name="todo" />
+                    <button className="add__todo__btn">Add New Task</button>
+                </form>
+            </section>
+            <section className="todolist">
+                <h1>ToDo List</h1>
+                <div className="todolist__btns">
+                    <button>All</button>
+                    <button>Done</button>
+                    <button>Todo</button>
+                </div>
+            </section>
+            <section className="todos__container">
+                <div className="todo">
+                    <div className="todo__title">
+                        <p>Hello World</p>
+                    </div>
+                    <div className="todos__btns">
+                        <label htmlFor="check"></label>
+                        <input type="checkbox" name="check" id="check" />
+                        <buttton>x</buttton>
+                        <buttton>x</buttton>
+                    </div>
+                </div>
+                <div className="todo">
+                    <div className="todo__title">
+                        <p>Hello World</p>
+                    </div>
+                    <div className="todos__btns">
+                        <label htmlFor="check"></label>
+                        <input type="checkbox" name="check" id="check" />
+                        <buttton>x</buttton>
+                        <buttton>x</buttton>
+                    </div>
+                </div>
+            </section >
+            <section className="delete__tasks">
+                <button>Delete done tasks</button>
+                <button>Delete all tasks</button>
+            </section>
+        </div >
+    );
+}
