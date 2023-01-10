6e50c35e5038d83e76a5dce54686f2a5b53dcb55
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-10 08:27:01 +0200

    Update date in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="footer">
-            <p>© 2022 - Borislav Benderov - All Rights Reserved</p>
+            <p>© {new Date().getFullYear()} - Borislav Benderov - All Rights Reserved</p>
         </footer>
     );
 }
